df4a968a98236f9b2f229fb27881d9c5e7a9b377
anng <99F2eDBMvfW9bHGzVycU>
2022-12-29 04:45:57 -0600

    Another necessary files

---
+++ app/src/main/java/com/example/rodrigo/sgame/ScreenSelectMusic/ArrayAdapterLevel.java
@@ -0,0 +1,59 @@
+package com.example.rodrigo.sgame.ScreenSelectMusic;
+
+import android.content.Context;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.rodrigo.sgame.CommonGame.Level;
+import com.example.rodrigo.sgame.R;
+
+import java.util.ArrayList;
+
+public class ArrayAdapterLevel extends ArrayAdapter<Level> {
+    int lastPosition =-1;
+    public ArrayAdapterLevel(@NonNull Context context, ArrayList<Level> resource) {
+        super(context, 0,resource);
+    }
+
+    public ArrayAdapterLevel( @NonNull Context context, int resource, int textViewResourceId) {
+        super(context, resource, textViewResourceId);
+    }
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView,  @NonNull ViewGroup parent) {
+        return initView(position, convertView, parent);
+    }
+
+    @Override
+    public View getDropDownView(int position,  @Nullable View convertView,  @NonNull ViewGroup parent) {
+        return initView(position, convertView, parent);
+    }
+
+    private View initView(int position,View convertedView,ViewGroup parent){
+
+        if(convertedView== null){
+            convertedView=LayoutInflater.from(getContext()).inflate(R.layout.lvl_item,parent,false);
+        }
+
+        ImageView imageView = convertedView.findViewById(R.id.bg_lvl);
+        TextView lvl = convertedView.findViewById(R.id.tv_level);
+        Level current = getItem(position);
+
+        if(current!=null){
+            lvl.setText(current.getNumberLevel()+"");
+        }
+
+
+        return  convertedView;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/rodrigo/sgame/ScreenSelectMusic/MusicThread.java
@@ -0,0 +1,61 @@
+package com.example.rodrigo.sgame.ScreenSelectMusic;
+
+import android.media.MediaPlayer;
+
+public class MusicThread extends Thread {
+
+    public MediaPlayer player;
+    public long time=20000000;
+    public  boolean isRunning = true;
+    long timeStamp;
+    public static String owoowoowo = "songs/piu/f2/o";
+
+
+
+
+
+    public MusicThread() {
+        timeStamp=System.currentTimeMillis();
+    }
+    public void changeSong(Double time,MediaPlayer mp) {
+        player.setVolume(1f, 1f);
+        this.time = (int) (time * 1000);
+       this.player=mp;
+
+    }
+
+
+    @Override
+    public void run() {
+        super.run();
+        while (isRunning) {
+            try {
+                if (time <= 3500 && time>(0)) {
+                    float volume = (float) time / 3500;
+                    if (player!=null && volume>=0 && volume<=1f){
+                        player.setVolume(volume, volume);
+                    }
+                    else if (player!=null && volume<0){
+                        player.setVolume(0, 0);
+                    }
+
+                }
+
+                    long resta=System.currentTimeMillis()-timeStamp;
+                    time-=resta;
+                    timeStamp=System.currentTimeMillis();
+
+                sleep(25);
+            } catch (IllegalStateException ex2){
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                try {
+                    this.join();
+                } catch (InterruptedException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+    }
+}
